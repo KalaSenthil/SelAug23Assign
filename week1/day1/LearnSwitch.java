dde50477b922a08a2ea38f8d3a4b9f603e5536e3
KalaSenthil <72850325+KalaSenthil@users.noreply.github.com>
2023-08-26 14:03:47 +0530

    Add files via upload
    Week1Day1 Assignments

---
+++ week1/day1/LearnSwitch.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class LearnSwitch {
+
+	public static void main(String[] args) {
+		String browser = "Chrome";
+		switch (browser) {
+		case "Chrome":System.out.println("Open Chrome Browser");break;
+
+		default:System.out.println("Default Browser");break;
+		}
+
+	}
+
+}
---
+++ week1/day1/PrimitiveDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		int num = 16;
+		long longerNum = 1237897789;
+		float flNum = 20.5f;
+		double dNum = 12.890234;
+		boolean isCorrect = true;
+		char oneChar = 'C';
+		String myStr = "Aim";
+		
+		System.out.println("Integer number is : "+num);
+		System.out.println("Long Type number is: "+longerNum);
+		System.out.println("Float Type number is: "+flNum);
+		System.out.println("Double Type number is: "+dNum);
+		System.out.println("Boolean Type is: "+isCorrect);
+		System.out.println("Char value is: "+oneChar);
+		System.out.println("String value is: "+myStr);
+	
+		
+		
+
+	}
+
+}
